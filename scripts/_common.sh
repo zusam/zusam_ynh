7b9baa3c97283d9db556bcbc9e6a56c222e9be9f
Niels Robin-Aubertin <git@niels.fr>
2019-08-14 15:05:56 +0200

    Remove generic comments from the install script

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,14 +7,7 @@
 # dependencies used by the app
 pkg_dependencies="unzip ffmpeg"
 php_version="7.3"
-php_extensions="\
-    php$php_version-curl \
-    php$php_version-imagick \
-    php$php_version-intl \
-    php$php_version-mbstring \
-    php$php_version-sqlite3 \
-    php$php_version-xml \
-"
+php_extensions="php$php_version-curl php$php_version-imagick php$php_version-intl php$php_version-mbstring php$php_version-sqlite3 php$php_version-xml"
 
 #=================================================
 # PERSONAL HELPERS
