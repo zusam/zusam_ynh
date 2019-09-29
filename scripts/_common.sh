85822789b5cbadab86fc88f1631634f53589dbe1
Jean-Baptiste Holcroft <jean-baptiste@holcroft.fr>
2019-09-29 07:26:14 +0200

    move additional sources in _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,3 +12,16 @@ php_extensions="php$php_version-curl php$php_version-imagick php$php_version-int
 #=================================================
 # PERSONAL HELPERS
 #=================================================
+
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+source ynh_add_extra_apt_repos__3
+source ynh_install_php__3
+source ynh_composer__2
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
